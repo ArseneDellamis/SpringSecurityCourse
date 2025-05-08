f0f739c2e6ddb15773cb7bc71be455182a0517e9
ArseneDellamis <dellamis11@gmail.com>
2025-05-08 17:25:49 +0200

    adding starter web in dependencies and make user extends userDetails and added a query to search a suer by username or email

--- src/main/java/com/security/springSeecurity/entity/User.java
+++ src/main/java/com/security/springSeecurity/entity/User.java
@@ -1,28 +1,80 @@
 package com.security.springSeecurity.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 @Data@Builder@AllArgsConstructor@NoArgsConstructor
 @Entity
 @Table(name = "users")
-public class User {
+public class User  implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
     private String fullName;
+
+    @Column(nullable = false)
     private String username;
+
+    @Column(nullable = false, unique = true, length = 100)
     private String email;
+
+    @Column(nullable = false)
     private String password;
+
+    @UpdateTimestamp
+    @Column(updatable = false, name = "created_at")
     private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(updatable = false, name = "updated_at")
     private Date updatedAt;
+
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
 }
--- src/main/java/com/security/springSeecurity/repository/UserRepository.java
+++ src/main/java/com/security/springSeecurity/repository/UserRepository.java
@@ -4,6 +4,10 @@ import com.security.springSeecurity.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository  extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsernameIgnoreCaseOrEmailIgnoreCase(String username, String email);
 }
