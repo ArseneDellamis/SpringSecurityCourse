1ec97013fb28e001e065d32f34db242ebe5880b8
ArseneDellamis <dellamis11@gmail.com>
2025-05-08 10:16:54 +0200

    adding project to branch jwt

---
+++ src/main/java/com/security/springSeecurity/SpringSeecurityApplication.java
@@ -0,0 +1,13 @@
+package com.security.springSeecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSeecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSeecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/security/springSeecurity/SpringSeecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.springSeecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSeecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
