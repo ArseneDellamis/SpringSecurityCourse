30ed5314e0eb144ca96a1c3e49cd19a1e0457db3
ArseneDellamis <dellamis11@gmail.com>
2025-05-15 10:39:02 +0200

    added user controller to retrieve the current logged in user

---
+++ src/main/java/com/security/springSeecurity/services/UserService.java
@@ -0,0 +1,25 @@
+package com.security.springSeecurity.services;
+
+import com.security.springSeecurity.entity.User;
+import com.security.springSeecurity.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<User> allUsers() {
+        List<User> users = new ArrayList<>();
+
+        userRepository.findAll().forEach(users::add);
+
+        return users;
+    }
+}
